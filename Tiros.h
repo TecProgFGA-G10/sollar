f284ae929da1f05e95c7a4b7936ea8e88b47b38c
cotrim <cotrim149@gmail.com>
2014-07-08 15:27:17 -0300

    TIros.h

--- Tiros.h
+++ Tiros.h
@@ -13,5 +13,19 @@ int posicaoVaziaTiros(itemDeJogo *tiros)
     return -1;//caso n�o encontra retorna -1;
 }
 
+void atualizaCaixaColisaoTiro(itemDeJogo *item)
+{
+    //� diferente pois o tiro se move em um valor diferente no eixo z.
+    int i =0;
+    for(i = 0; i < 8; i++)//tem 8 pontos da caixa de colisao.
+    {
+        if(item->posicao.z > item->posicaoAnterior.z)
+            item->colisao.pontos[i].z+= -item->aceleracao;//(item->posicao.x - item->posicaoAnterior.x);
+
+        if(item->posicao.z < item->posicaoAnterior.z)
+            item->colisao.pontos[i].z+= item->aceleracao;//(item->posicao.x - item->posicaoAnterior.x);
+    }
+}
+
 
 #endif
