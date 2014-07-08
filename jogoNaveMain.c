97caf6436898bfd5d11265baefb5b5d6b69c0051
cotrim <cotrim149@gmail.com>
2014-07-08 14:09:47 -0300

    Retirado posicao vazia de Explosao, Meteoro e Tiros, e enviaMeteoro

--- jogoNaveMain.c
+++ jogoNaveMain.c
@@ -8,6 +8,9 @@
 #include "GerenciadorGrafico.h"
 #include "GerenciadorAudio.h"
 #include "Colisao.h"
+#include "Meteoro.h"
+#include "Tiros.h"
+#include "Explosao.h"
 
 #define NUM_MAX_TIROS 10 //número máximo de tiros que podem ser disparados
 #define NUM_MAX_METEOROS 20 //número máximo de meteoros que podem vir
@@ -78,16 +81,11 @@ GLubyte cTGAcompare[12] = {0,0,10,0,0,0,0,0,0,0,0,0};	// Compressed TGA Header
 void desenhaCaixaColisao(caixaColisao *);
 void criaCaixaColisao(GLMmodel *, caixaColisao *);
 void inicializaObjetos(void);
-void enviaMeteoro(void);
 void setaCaixaColisao(caixaColisao *, ponto);
 void atualizaCaixaColisao(itemDeJogo *);
 void atualizaCaixaColisaoTiro(itemDeJogo *);
 void desenhaGameOver(void);
 void atualizarEstados(void);
-int verificaColisao(caixaColisao , caixaColisao );
-int posicaoVaziaMeteoros(void);
-int posicaoVaziaTiros(void);
-int posicaoVaziaExplosoes(void);
 void configuraTexturas(void);
 void desenhaMeteoros(void);
 void desenhaExplosoes();
@@ -294,39 +292,6 @@ void inicializaObjetos(void)
     }
 }
 
-void enviaMeteoro(void)
-{
-    int i =0;
-    for(i = 0; i < meteorosEnviar; i++)//envia meteorosEnviar meteoros
-    {
-       int pos = posicaoVaziaMeteoros();
-        if(pos >=0)//se tem uma pos disponivel..
-        {
-            meteoros[pos].visivel = TRUE;
-            meteoros[pos].posicao.z = (Z_INICIAL_METEORO-10) - rand()%(Z_INICIAL_METEORO - -20);
-            meteoros[pos].posicao.x = MINIMO_X + rand()%(MAXIMO_X-MINIMO_X);
-            meteoros[pos].posicao.y = MINIMO_Y + rand()%(MAXIMO_Y-MINIMO_Y) ;
-            //guarda a posição anterior para calcular a caixa de colisão para o metoro
-            //em uma posição diferente da <0,0,0>
-            meteoros[pos].posicaoAnterior.z = meteoros[pos].posicao.z;
-            meteoros[pos].posicaoAnterior.x = meteoros[pos].posicao.x;
-            meteoros[pos].posicaoAnterior.y = meteoros[pos].posicao.y;
-
-            int c =0;
-            for(c =0; c< 8; c++)
-            //configura a caixa de colisão, para um meteoro na posição <0,0,0>
-            {
-                meteoros[pos].colisao.pontos[c].x = colisaoMeteoroDefault.pontos[c].x;
-                meteoros[pos].colisao.pontos[c].y = colisaoMeteoroDefault.pontos[c].y;
-                meteoros[pos].colisao.pontos[c].z = colisaoMeteoroDefault.pontos[c].z;
-            }
-            //calcula a posição da caixa para posição do meteoro.
-            setaCaixaColisao(&meteoros[pos].colisao, meteoros[pos].posicao);
-        }
-    }
-
-}
-
 void desenhaGameOver()
 {
     //desativa as callbacks de teclado do jogo..
@@ -440,7 +405,7 @@ void atualizarEstados(void)
                         PlaySound(MODELO_EXPLOSAO,somExplosao);
                         meteoros[m].visivel = FALSE;
                         tiros[i].visivel = FALSE;
-                        int explos = posicaoVaziaExplosoes();
+                        int explos = posicaoVaziaExplosoes(explosoes);
                         pontos+= VALOR_PONTO;
                         if(explos>=0)
                         {
@@ -467,7 +432,7 @@ void atualizarEstados(void)
                 PlaySound(MODELO_EXPLOSAO,somExplosao);
                 meteoros[i].visivel = FALSE;
                 vida--;
-                int explos = posicaoVaziaExplosoes();
+                int explos = posicaoVaziaExplosoes(explosoes);
                 if(explos>=0)
                 {
                     explosoes[explos].posicao.x = meteoros[i].posicao.x;
@@ -489,40 +454,7 @@ void atualizarEstados(void)
     }
 }
 
-int posicaoVaziaMeteoros(void)
-{
-    int i =0;
-    for(i = 0; i < NUM_MAX_METEOROS; i++)
-    {
-        //encontra o primeiro meteoro vazio e o retorna.
-        if(!meteoros[i].visivel)
-            return i;
-    }
-    return -1;//caso não encontra retorna -1;
-}
 
-int posicaoVaziaTiros(void)
-{
-    int i =0;
-    for(i = 0; i < NUM_MAX_TIROS; i++)
-    {
-        //encontra o primeiro tiro vazio e o retorna.
-        if(!tiros[i].visivel)
-            return i;
-    }
-    return -1;//caso não encontra retorna -1;
-}
-
-int posicaoVaziaExplosoes(void)
-{
-    int i =0;
-    for(i = 0; i < NUM_MAX_METEOROS; i++)
-    {
-        if(!explosoes[i].visivel)
-            return i;
-    }
-    return -1;//caso não encontra retorna -1;
-}
 //trata as teclas de game over, para teclas especiais ESQUERDA e DIREITA. ao pressionar uma destes
 //seleciona se quer continuar ou não o game.
 void teclaGameOverEspecial(int tecla, int x, int y)
@@ -830,7 +762,7 @@ void trataTeclas()
     {
         PlaySound(MODELO_TIRO,somTiro);
         atirar = FALSE;
-        int tiro = posicaoVaziaTiros();//pega a primeira pos vazia.
+        int tiro = posicaoVaziaTiros(tiros);//pega a primeira pos vazia.
 
         if(tiro >=0)
         {
@@ -1091,7 +1023,7 @@ void timer(int t)//timer para invocar um/uns novo/novos metoro/meteoros
             printf("novo meteoro");
             if(t == NOVO_METEORO)
             {
-                enviaMeteoro();
+                enviaMeteoro(meteoros,meteorosEnviar,colisaoMeteoroDefault);
                 glutTimerFunc(tempoMeteoro,  timer,  NOVO_METEORO);
             }
         }
