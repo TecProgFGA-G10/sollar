453c1f06546fa406d0821fd166fb5b3986bb811b
Eduardo Vital <vitaldu@gmail.com>
2015-03-29 21:27:27 -0300

    Apply style and desing in GerenciadorAudio.h archive

--- GerenciadorAudio.h
+++ GerenciadorAudio.h
@@ -4,24 +4,26 @@
 #include <SDL/SDL_mixer.h>
 #include "SolarUtil.h"
 
-void iniciaAudio(int audio_rate, Uint16 audio_format, int audio_channels, int audio_buffers){
-   if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0)
-		{
-				printf("Unable to initialize audio: %s\n", Mix_GetError());
-		}
-
+void iniciaAudio(int audio_rate,
+                 Uint16 audio_format,
+                 int audio_channels,
+                 int audio_buffers)
+{
+	if (Mix_OpenAudio(audio_rate, audio_format,
+	                 audio_channels, audio_buffers) != 0)
+	{
+		printf("Unable to initialize audio: %s\n", Mix_GetError());
+	}
 }
 
 
 int tocaMusica(Mix_Music *musica)
 {
-	if(musica == NULL)
-	{
+	if (musica == NULL) {
 		printf("Unable to load Ogg file: %s\n", Mix_GetError());
 		return 1;
 	}
-	if(Mix_PlayMusic( musica, -1) == -1)
-	{
+	if (Mix_PlayMusic(musica, -1) == -1) {
 		printf("Unable to play Ogg file: %s\n", Mix_GetError());
 	}
 	return 0;
@@ -29,12 +31,10 @@ int tocaMusica(Mix_Music *musica)
 
 void PlaySound(int som,Mix_Chunk *somEfeito)
 {
-	if(som == MODELO_TIRO)
-	{
+	if (som == MODELO_TIRO) {
 		Mix_PlayChannel(2,somEfeito,0);
 	}
-	else if(som == MODELO_EXPLOSAO)
-	{
+	else if (som == MODELO_EXPLOSAO) {
 		Mix_PlayChannel(3,somEfeito,0);
 	}
 
@@ -43,11 +43,10 @@ void PlaySound(int som,Mix_Chunk *somEfeito)
 void limpa(Mix_Music *musica)
 {
 	Mix_HaltMusic();
-		Mix_FreeMusic(musica);
-		musica = NULL;
-		Mix_HaltChannel(2);
-		Mix_HaltChannel(3);
+	Mix_FreeMusic(musica);
+	musica = NULL;
+	Mix_HaltChannel(2);
+	Mix_HaltChannel(3);
 }
-
 
 #endif
