893216b82df71a8743f03996e5cad93c23ddaf1c
João Paulo Ferreira <iamferreirajp@gmail.com>
2015-04-14 09:20:22 -0300

    Revised stylesheet and commented functions in: GerenciadorAudio.h Fixed tabs in Game.h

--- GerenciadorAudio.h
+++ GerenciadorAudio.h
@@ -1,6 +1,7 @@
-/*Solar 3D Técnicas de Programação
-*Esse programa inicializa os sons e 
-*toca os sons do jogo
+/*
+* Solar 3D Técnicas de Programação
+* Esse programa inicializa os sons e 
+* toca os sons do jogo
 */
 
 #ifndef GERENCIADOR_AUDIO_H
@@ -9,31 +10,42 @@
 #include <SDL/SDL_mixer.h>
 #include "SolarUtil.h"
 
+/* initializes the sound */
 void iniciaAudio(int audio_rate,
-                 Uint16 audio_format,
-                 int audio_channels,
-                 int audio_buffers)
+				 Uint16 audio_format,
+				 int audio_channels,
+				 int audio_buffers)
 {
 	if (Mix_OpenAudio(audio_rate, audio_format,
-	                  audio_channels, audio_buffers) != 0)
+					  audio_channels, audio_buffers) != 0)
 	{
 		printf("Unable to initialize audio: %s\n", Mix_GetError());
 	}
+	else {
+		/* nothing to do */
+	}
 }
 
-
+/* plays the music */
 int tocaMusica(Mix_Music *musica)
 {
 	if (musica == NULL) {
 		printf("Unable to load Ogg file: %s\n", Mix_GetError());
 		return 1;
 	}
+	else {
+		/* nothing to do */
+	}
 	if (Mix_PlayMusic(musica, -1) == -1) {
 		printf("Unable to play Ogg file: %s\n", Mix_GetError());
+	}
+	else {
+		/* nothing to do */
 	}
 	return 0;
 }
 
+/* plays effects sound */
 void PlaySound(int som,Mix_Chunk *somEfeito)
 {
 	if (som == MODELO_TIRO) {
@@ -45,6 +57,7 @@ void PlaySound(int som,Mix_Chunk *somEfeito)
 
 }
 
+/* clear audio */
 void limpa(Mix_Music *musica)
 {
 	Mix_HaltMusic();
