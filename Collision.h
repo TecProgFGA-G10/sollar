375783d1cf9e1e8e1ce6f6e177639f0f699d8b53
Eduardo Vital <vitaldu@gmail.com>
2015-05-21 23:58:28 -0300

    Added log into function lesser_value

--- Collision.h
+++ Collision.h
@@ -42,14 +42,18 @@ float greater_value(float value_a, float value_b)
 /* return the lesse value of two values */
 float lesser_value(float value_a, float value_b)
 {
-	float result_lesser_value = INITIALIZE_ZERO;
-	if (value_a < value_b) {
-		result_lesser_value = value_a;
-	}
-	else {
-		result_lesser_value = value_b;
-	}
-	return result_lesser_value;
+  float result_lesser_value = INITIALIZE_ZERO;
+  if (value_a < value_b) {
+    result_lesser_value = value_a;
+    print_debug_log("Value  %f is lower than %f",
+                    result_greater_value, value_b);
+  }
+  else {
+    result_lesser_value = value_b;
+    print_debug_log("Value  %f is lower than %f",
+                    result_greater_value, value_a);
+  }
+  return result_lesser_value;
 }
 
 /* updates the points of the box in collision */
