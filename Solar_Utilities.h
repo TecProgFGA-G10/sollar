69889eae2b3a3b93839f673da60902760e2b83c5
fagner <fanig_01@hotmail.com>
2015-06-30 17:27:56 -0300

    Include call function Check_pixels_for_image_data in the Solar_Utilities.h arquive

--- Solar_Utilities.h
+++ Solar_Utilities.h
@@ -471,13 +471,14 @@ int LoadCompressedTGA(Texture *texture, char *filename, FILE *fTGA)
 				texture->imageData[currentbyte + 1] = colorbuffer[1];
 				texture->imageData[currentbyte + 2] = colorbuffer[0];
 
-				if (tga.bytesPerPixel == 4) {
+				/*if (tga.bytesPerPixel == 4) {
 					texture->imageData[currentbyte + 3] = colorbuffer[3];
 					//print_verbose_log("imageData received bytesPerPixel");
 				}
 				else {
 					//print_error_log("Error, not possible receives bytesPerPixel");
-				}
+				}*/
+				texture->imageData[currentbyte + 3] = Check_pixels_for_image_data(tga.bytesPerPixel, colorbuffer);
 
 				currentbyte += tga.bytesPerPixel;
 				currentpixel++;
